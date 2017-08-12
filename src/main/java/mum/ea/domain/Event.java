10fd6cd07d77c33947f426f3c74a496c533da971
ChandaraLeang <leangchandara@gmail.com>
2017-08-11 21:58:34 -0500

    update Event domain and EventService

--- src/main/java/mum/ea/domain/Event.java
+++ src/main/java/mum/ea/domain/Event.java
@@ -1,25 +1,32 @@
 package mum.ea.domain;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
 
 @Entity
 public class Event {
 	
 	@Id @GeneratedValue
 	private int id;
+	@NotNull
 	@Column
 	private String name;
+	@NotNull
 	@Column
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date startDate;
+	@NotNull
 	@Column
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date endDate;
@@ -27,10 +34,28 @@ public class Event {
 	@ManyToOne
 	private Facility facility;
 	
+	@NotNull
 	@Column
 	private int noOfPeople;
 	@Column
 	private boolean status;
+	
+	@OneToMany
+	private List<Category> categories = new ArrayList<>();
+	
+	public Event(){
+		
+	}
+	
+	public Event(String name, Date startDate, Date endDate, Facility facility, int noOfPeople, boolean status, List<Category> categories){
+		this.name = name;
+		this.startDate = startDate;
+		this.endDate = endDate;
+		this.facility = facility;
+		this.noOfPeople = noOfPeople;
+		this.status = status;
+		this.categories = categories;
+	}
 	
 	public int getId() {
 		return id;
@@ -86,5 +111,13 @@ public class Event {
 
 	public void setStatus(boolean status) {
 		this.status = status;
+	}
+
+	public List<Category> getCategories() {
+		return categories;
+	}
+
+	public void setCategories(List<Category> categories) {
+		this.categories = categories;
 	}
 }
