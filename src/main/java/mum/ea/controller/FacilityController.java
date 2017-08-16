758d2f4d8a026b38fbfe17d18f21b8114cf7b92e
krishnaaGit <krishnaa.karmacharya@gmail.com>
2017-08-16 08:22:46 -0500

    Event Facility booked validation

--- src/main/java/mum/ea/controller/FacilityController.java
+++ src/main/java/mum/ea/controller/FacilityController.java
@@ -10,10 +10,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import mum.ea.domain.Facility;
 import mum.ea.domain.FacilityType;
@@ -26,22 +29,16 @@ public class FacilityController {
 
 	@Autowired
 	private FacilityService facilityService;
-	
+
 	@Autowired
 	private BuildingService buildingService;
-
-	/*@GetMapping("/")
-	public String index(Model model) {
-		model.addAttribute("fullPageMessage", "Greetings from Spring Boot!");
-		return "fullPageMessage";
-	}*/
 
 	@GetMapping("/facilities")
 	public String getFacilitys(Model m) {
 		m.addAttribute("facilities", facilityService.getAllFacilities());
 		return "facilityList";
 	}
-	
+
 	@GetMapping("/buildings/{buildingId}/facilities")
 	public String getBuildingFacilities(@PathVariable int buildingId, Model m) {
 		m.addAttribute("facilities", facilityService.findByBuilding(buildingId));
@@ -65,24 +62,50 @@ public class FacilityController {
 	}
 
 	@PostMapping("/facilities")
-	public String addFacility(@Valid Facility b, BindingResult result) {
+	public String addFacility(@ModelAttribute("facility") @Validated Facility b, Model model, BindingResult result,
+			final RedirectAttributes redirectAttributes) {
 		if (result.hasErrors() || facilityService.existFacilityCode(b)) {
 			return "facilityDetail";
 		}
 
 		else {
+
 			facilityService.addFacility(b);
+			redirectAttributes.addFlashAttribute("css", "success");
+			if (b.getId() == 0) {
+				redirectAttributes.addFlashAttribute("msg", "Facility added successfully!");
+			} else {
+				redirectAttributes.addFlashAttribute("msg", "Facility updated successfully!");
+			}
 			return "redirect:/admin/facilities";
 		}
 	}
 
 	@PostMapping("/facilities/{id}")
-	public String updateFacility(@Valid Facility b, BindingResult result) {
+	public String updateFacility(@PathVariable Integer id, Model m, @ModelAttribute("facility") @Validated Facility b,
+			BindingResult result, final RedirectAttributes redirectAttributes) {
+		m.addAttribute("errorMsg", "");
+		m.addAttribute("buildings", buildingService.getAllBuildings());
 		if (result.hasErrors()) {
 			return "facilityDetail";
+		} else if (id != facilityService.existingFacilityCode(b)) {
+			m.addAttribute("errorMsg", "Facility Code Already Exists");
+			return "facilityDetail";
 		} else {
-			facilityService.updateFacility(b);
-			return "redirect:/admin/facilities";
+			try {
+
+				facilityService.updateFacility(b);
+				redirectAttributes.addFlashAttribute("css", "success");
+				if (id == 0) {
+					redirectAttributes.addFlashAttribute("msg", "Facility added successfully!");
+				} else {
+					redirectAttributes.addFlashAttribute("msg", "Facility updated successfully!");
+				}
+				return "redirect:/admin/facilities";
+			} catch (Exception ex) {
+				System.out.println("through catch");
+				return "facilityDetail";
+			}
 		}
 	}
 
